231be903b3451634143522ac6402418e569160b3
Suhas Shetty <suhas.shetty@galepartners.com>
2017-12-08 19:40:19 +0530

    Addition of Itineraries list page

---
+++ KtMobileAppSource/KtMobileApp/KtMobileApp/Views/Itineraries.xaml.cs
@@ -0,0 +1,33 @@
+﻿using KtMobileApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KtMobileApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Itineraries : ContentPage
+	{
+        ItinerariesViewModel viewModel;
+
+        public Itineraries ()
+		{
+			InitializeComponent ();
+
+            BindingContext = viewModel = new ItinerariesViewModel();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.ItineraryDailyBreakDown.Count == 0)
+                viewModel.LoadItineraryCommand.Execute(null);
+        }
+    }
+}
