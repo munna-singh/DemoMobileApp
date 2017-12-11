069d4582e935a2022d78b6269a00d9005de70878
vinod <vinod.gangwar@gmail.com>
2017-12-11 10:55:50 +0530

    Added itinerary detailed viewe page

--- KtMobileAppSource/KtMobileApp/KtMobileApp/Models/ItineraryDailyBreakDown.cs
+++ KtMobileAppSource/KtMobileApp/KtMobileApp/Models/ItineraryDailyBreakDown.cs
@@ -1,14 +1,27 @@
 ﻿using KtMobileApp.ViewModels;
+using KtMobileApp.Views;
+//using KtMobileApp.Views;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace KtMobileApp.Models
 {
-    public class ItineraryDailyBreakDown: BaseViewModel
+    public class ItineraryDailyBreakDown : BaseViewModel
     {
+        private INavigation _pageNavigation;
+
+        public ItineraryDailyBreakDown(INavigation MainPageNavigation)
+        {
+            Title = "Itinerary Name";
+            _pageNavigation = MainPageNavigation;
+            //DeleteCommand = new Command<ItineraryDailyBreakDown>(async (model) => await DeleteExec(model));
+            OpenItineraryDayDetails = new Command<ItineraryDailyBreakDown>(async (model) => await OpenNewPage(model));
+        }
+
         public string ImageUri { get; set; }
         public string Location { get; set; }
         public string TripDayDate { get; set; }
@@ -16,18 +29,47 @@ namespace KtMobileApp.Models
         public string IsPastText { get; set; }
         public int DayNumber { get; set; }
         public string CheckImageUri { get; set; }
-
+        public string ItineraryDayDescription { set; get; }
         public string ImageResourceActivityPath { set; get; }
-        
+
         private string _imageResourcePassedPath;
         public string ImageResourcePassedPath
         {
             get { return _imageResourcePassedPath; }
-            set {
-                if (IsPast)
+            set
+            {               
                     _imageResourcePassedPath = value;
             }
         }
+
+        private bool _expandItineraryDayDetails;
+        public bool ExpandItineraryDayDetails
+        {
+            get
+            {
+                return _expandItineraryDayDetails;
+            }
+            set
+            {
+                _expandItineraryDayDetails = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public int ImageSourcePastTripDayHide
+        {
+            get {
+                return IsPast ? 100 : 0;//for seting the opacity of image value = 0 (means not visible, 100 means full visible)
+            }           
+        }        
+
+        public Command OpenItineraryDayDetails { get; set; }
+        
+        public async Task OpenNewPage(ItineraryDailyBreakDown objVm)
+        {            
+            await _pageNavigation.PushAsync(new ItineraryCompleteDetails(objVm)); //_pageNavigation.PushModalAsync(new Page) ; NOTE: this is for Modal Dialog
+        }
+
 
         public Color CurrentTripDaySelectedItem { get; set; }
 
@@ -39,13 +81,40 @@ namespace KtMobileApp.Models
             }
         }
 
-        public ImageSource ImageSourcePassed
+        public ImageSource ImageSourcePastTripDay
         {
             get
             {
                 return ImageSource.FromResource(ImageResourcePassedPath);
             }
         }
+
+        private int _ExpandingHeight = 0;
+        public int ExpandedHeight
+        {
+            get
+            {
+                return _ExpandingHeight;
+            }
+            set
+            {
+                _ExpandingHeight = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public Command ExpandHideData
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    ExpandItineraryDayDetails = !ExpandItineraryDayDetails;
+                });
+            }
+        }
+
     }
 
 }
---
+++ KtMobileAppSource/KtMobileApp/KtMobileApp/Views/ItineraryCompleteDetails.xaml.cs
@@ -0,0 +1,34 @@
+﻿using KtMobileApp.Models;
+using KtMobileApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace KtMobileApp.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ItineraryCompleteDetails : ContentPage
+	{
+        private ItineraryCompleteDetailsViewModel viewModel;
+        public ItineraryCompleteDetails (ItineraryDailyBreakDown parentViewModel)
+		{
+			InitializeComponent ();
+
+            viewModel = new ItineraryCompleteDetailsViewModel();
+            viewModel.DayNumber = parentViewModel.DayNumber;
+            viewModel.DayDate = parentViewModel.TripDayDate;
+            viewModel.LocationName = parentViewModel.Location;
+            viewModel.ImageBanner = "KtMobileApp.Assets.Images.BannerImage_2_256_256.png";
+            viewModel.CompleteDescription = parentViewModel.ItineraryDayDescription;
+
+
+
+            BindingContext = viewModel;
+        }
+	}
+}
